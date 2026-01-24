98c796f47e260db56bdf5ba470a3278ceb85f888
kevin <contactohuayta@gmail.com>
2026-01-24 17:45:18 -0300

    modifique el archivo key de firebase para que se ejecute con render

--- src/main/java/com/affinityteach/firebase/FirebaseInitializer.java
+++ src/main/java/com/affinityteach/firebase/FirebaseInitializer.java
@@ -1,5 +1,7 @@
 package com.affinityteach.firebase;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -18,8 +20,18 @@ public class FirebaseInitializer {
 
 	@PostConstruct
 	public void initFirestore() throws IOException {
-		InputStream serviceAccount = 
-				getClass().getClassLoader().getResourceAsStream("private-key-firestore.json");
+		String filePath = "/etc/secrets/private-key-firestore.json";
+		File file = new File(filePath);
+		
+		InputStream serviceAccount;
+		if (file.exists()) {
+			// archivo desde /etc/secrets/
+			serviceAccount = new FileInputStream(file);
+		} else {
+			//resources (desarrollo local que por el momento no esta habilitado)
+			serviceAccount = getClass().getClassLoader()
+					.getResourceAsStream("private-key-firestore.json");
+		}
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
 				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
