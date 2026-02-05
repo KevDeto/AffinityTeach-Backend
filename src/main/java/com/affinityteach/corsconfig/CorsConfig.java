36010cc174242a2b90a491e6e0097fef67f7b2f6
kevin <contactohuayta@gmail.com>
2026-02-05 13:02:11 -0300

    agregue un cors para vercel

--- src/main/java/com/affinityteach/corsconfig/CorsConfig.java
+++ src/main/java/com/affinityteach/corsconfig/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins(
+                        		"http://localhost:5173", 
+                        		"http://localhost:3000", 
+                        		"https://affinity-teach.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
