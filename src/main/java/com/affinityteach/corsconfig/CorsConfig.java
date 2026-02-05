2c9eb74b173bd45ba98daccb054cecac3be48f4b
kevin <contactohuayta@gmail.com>
2026-02-05 13:42:47 -0300

    com

--- src/main/java/com/affinityteach/corsconfig/CorsConfig.java
+++ src/main/java/com/affinityteach/corsconfig/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                         .allowedOrigins(
                         		"http://localhost:5173", 
                         		"http://localhost:3000", 
-                        		"https://affinity-teach.vercel.app/")
+                        		"https://affinity-teach.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
